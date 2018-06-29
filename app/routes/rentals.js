a3293ea5b1713a955357099adf65f7030f28a3a8
Edson Samuel Jr <samuedson@gmail.com>
2018-06-29 19:20:27 -0300

    Generate rental-tile component, extracting its template from rentals.hbs

--- app/routes/rentals.js
+++ app/routes/rentals.js
@@ -7,7 +7,7 @@ export default Route.extend({
       "title": "Grand Old Mansion",
       "owner": "Veruca Salt",
       "location": "San Francisco",
-      "category": "Estate",
+      "type": "Estate",
       "bedrooms": 15,
       "image": "https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg",
       "description": "This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests."
@@ -16,7 +16,7 @@ export default Route.extend({
       "title": "Urban Living",
       "owner": "Mike TV",
       "location": "Seattle",
-      "category": "Condo",
+      "type": "Condo",
       "bedrooms": 1,
       "image": "https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg",
       "description": "A commuters dream. This rental is within walking distance of 2 bus stops and the Metro."
@@ -25,7 +25,7 @@ export default Route.extend({
       "title": "Downtown Charm",
       "owner": "Violet Beauregarde",
       "location": "Portland",
-      "category": "Apartment",
+      "type": "Apartment",
       "bedrooms": 3,
       "image": "https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg",
       "description": "Convenience is at your doorstep with this charming downtown rental. Great restaurants and active night life are within a few feet."
