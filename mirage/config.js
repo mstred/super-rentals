1809436a3621e5b68ade742f0a782535b43ee34c
Edson Samuel Jr <samuedson@gmail.com>
2018-06-29 19:07:22 -0300

    Copy rentals' data to the mirage config

--- mirage/config.js
+++ mirage/config.js
@@ -23,4 +23,46 @@ export default function() {
 
     http://www.ember-cli-mirage.com/docs/v0.3.x/shorthands/
   */
+
+  this.namespace = '/api'
+
+  let rentals = [{
+    "id": "grand-old-mansion",
+    "type": "rentals",
+    "attributes": {
+      "title": "Grand Old Mansion",
+      "owner": "Veruca Salt",
+      "location": "San Francisco",
+      "category": "Estate",
+      "bedrooms": 15,
+      "image": "https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg",
+      "description": "This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests."
+    }
+  }, {
+    "id": "urban-living",
+    "type": "rentals",
+    "attributes": {
+      "title": "Urban Living",
+      "owner": "Mike TV",
+      "location": "Seattle",
+      "category": "Condo",
+      "bedrooms": 1,
+      "image": "https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg",
+      "description": "A commuters dream. This rental is within walking distance of 2 bus stops and the Metro."
+    }
+  }, {
+    "id": "downtown-charm",
+    "type": "rentals",
+    "attributes": {
+      "title": "Downtown Charm",
+      "owner": "Violet Beauregarde",
+      "location": "Portland",
+      "category": "Apartment",
+      "bedrooms": 3,
+      "image": "https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg",
+      "description": "Convenience is at your doorstep with this charming downtown rental. Great restaurants and active night life are within a few feet."
+    }
+  }];
+
+  this.get('/rentals', () => { return { "data": rentals }; });
 }
